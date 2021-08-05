9d8feebbfd660dc3d03166977c7421752ede199b
Spencer Mattison <spenceramattison@gmail.com>
2021-08-05 21:01:06 +0900

    created sample seed skeleton

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Plant.create!(
+  name: "",
+  scientific_name: "",
+  description: "",
+  cover_picture: "",
+  care: "",
+  temperature: "",
+  light: 1,
+  watering_schedule: 1440 ,
+  toxicity: ""
+)
