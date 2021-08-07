85061c619823a2933594839286b69524b223c8a8
Dean Abbott <75708832+HundiBar@users.noreply.github.com>
2021-08-07 16:54:38 +0900

    Update pots_controller.rb

--- app/controllers/pots_controller.rb
+++ app/controllers/pots_controller.rb
@@ -1,41 +1,37 @@
 class PotsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
-  before_action :set_plant, only: [:show]
+  before_action :set_pot, only: [:show, :destroy]
 
   def index
     @pots = policy_scope(Pot).order(created_at: :desc)
   end
 
-  def show
-    @pot = Pot.find(params[:id])
-  end
-
-  private
-
-  def set_plant
-    @plant = Plant.find(params[:id])
-    authorize @plant
-  end
+  def show; end
   
   def create
+    @plant = Plant.find(params[:id])
     @pot = Pot.new(pot_params)
-    @plant = Plant.find(pot_params[:plant_id])
     @pot.plant = @plant
     @pot.user = current_user
+    authorize @pot
     @pot.save
     redirect_to pots_path
   end
   
   def destroy
-    @pot = Pot.find(params[:id])
     @pot.destroy
     redirect_to plant_path
   end
 
   private
 
-  def pot_params
-    params.require(:pot).permit(:plant_id)
+  def set_pot
+    @pot = Pot.find(params[:id])
+    authorize @pot
   end
-
+    
+  def pot_params
+  params.require(:pot).permit(:plant_id)
+  end
+  
 end
