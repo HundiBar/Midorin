c0faf05fa7c66efdd6a6af5a6eccb227b4abb262
Spencer Mattison <spenceramattison@gmail.com>
2021-08-07 15:23:58 +0900

    created create controller

--- app/controllers/pots_controller.rb
+++ app/controllers/pots_controller.rb
@@ -5,4 +5,19 @@ class PotsController < ApplicationController
   def show
     @pot = Pot.find(params[:id])
   end
+
+  def create
+    @pot = Pot.new(pot_params)
+    @plant = Plant.find(params[:plant_id])
+    @pot.plant = @plant
+    @pot.save
+    redirect_to pots_path
+  end
+
+  private
+
+  def pot_params
+    params.require(:pot)
+  end
+
 end
