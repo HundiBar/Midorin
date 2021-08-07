1237b06fd650f043087d9b7d084a6880bbe0ad1b
Mylene Pont <pont.mylene@gmail.com>
2021-08-07 16:32:02 +0900

    destroy method done, waiting for test

--- app/controllers/pots_controller.rb
+++ app/controllers/pots_controller.rb
@@ -2,7 +2,14 @@ class PotsController < ApplicationController
   def index
     @pots = Pot.all.order(created_at: :desc)
   end
+
   def show
     @pot = Pot.find(params[:id])
+  end
+
+  def destroy
+    @pot = Pot.find(params[:id])
+    @pot.destroy
+    redirect_to plant_path
   end
 end
