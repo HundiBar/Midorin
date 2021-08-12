027abc323407f6a4f1f0e1ff0b244a4ee2617cfd
Spencer Mattison <spenceramattison@gmail.com>
2021-08-12 20:03:26 +0900

    added enumerables for plants

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,8 @@
 class Plant < ApplicationRecord
   has_many :pots
   enum light: { low: "low", medium: "medium", high: "high" }
+  enum watering_schedule: { high: 1, medium: 2, low: 3 }
+
 
   # validates :name, :description, :cover_picture, :watering_schedule, :care, presence: true
 
