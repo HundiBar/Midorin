143ea4b9531ac594706bb38154bcd4a9f467b988
Dean Abbott <deanabbott293@gmail.com>
2021-08-12 16:42:57 +0900

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # post '/plants', to: 'plants#index'
   get '/dashboards', to: 'user#dashboard'
-  get '/plants/search', to: 'plants#search'
+  # get '/plants/search', to: 'plants#search'
   get '/plants/filter', to: 'plants#filter'
-  post '/plants', to: 'plants#search_results'
 
 
   resources :plants, only: [:index, :show]
