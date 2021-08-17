28a19ebaf2ac2ace4366d9bab16200b026a0413e
Dean Abbott <deanabbott293@gmail.com>
2021-08-17 16:26:02 +0900

    test hash put in place to save api calls

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -24,9 +24,9 @@ class PlantsController < ApplicationController
       @plants = policy_scope(Plant).search_by_name(params[:query]).order(created_at: :desc)
 
     elsif @api_scan
-      api_call = ApiCall.new(params[:plant][:image])
-      # plant_search_name = TestHash["suggestions"].first["plant_name"]
-      plant_search_name = api_call.call_api
+      # api_call = ApiCall.new(params[:plant][:image])
+      plant_search_name = TestHash["suggestions"].first["plant_name"]
+      # plant_search_name = api_call.call_api
       @plants = policy_scope(Plant).search_by_name(plant_search_name)
     end
   end
