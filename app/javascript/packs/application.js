3308b159ec3437a16691388ad15e94e31abeb983
Dean Abbott <deanabbott293@gmail.com>
2021-08-19 12:22:54 +0900

    added JS to auto fire pic upload

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,9 +26,12 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { searchFire } from '../components/search_fire';
+import { picFire } from "../components/pic_upload";
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   searchFire();
+  picFire();
 });
