bee1e3b1cef4fdf2c1a83c1965b85412eee653b3
Troy Russell <troyjruss@gmail.com>
2021-08-21 15:57:23 +0900

    show page ok

--- app/javascript/components/confetti.js
+++ app/javascript/components/confetti.js
@@ -3,11 +3,22 @@ import party from 'party-js';
 const confetti = () => {
   const cake = document.querySelector('.cake');
 
+  if (cake) {
   cake.addEventListener('click', () => {
     party.confetti(cake, {
-          count: (200),
+          count: 50,
+          spread: 40
         });
-  });
+    party.confetti(cake, {
+          count: 50,
+          spread: 40,
+          shapes: ["star"]
+        });
+    party.sparkles(cake, {
+        });
+    cake.classList.add("cake-fadeout");
+    });
+  }
 }
 
 export { confetti };
