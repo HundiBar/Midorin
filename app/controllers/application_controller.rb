51ded86ab5e98cdc585ba67cd612fe778676638f
Spencer Mattison <spenceramattison@gmail.com>
2021-08-21 11:42:53 +0900

    updated line bot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
+require 'line/bot'
+
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :callback
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
@@ -11,8 +13,26 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: [:index, :callback], unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  protect_from_forgery with: :exception
+
+  before_action :validate_signature, only: :callback
+  def validate_signature
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+  end
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    }
+  end
 
   private
 
--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,37 +1,8 @@
 class LinebotController < ApplicationController
- require 'line/bot'  # gem 'line-bot-api'
 
-def client
-  @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  }
-end
+  protect_from_forgery except: :sort
 
-post '/callback' do
-  body = request.body.read
-
-  signature = request.env['HTTP_X_LINE_SIGNATURE']
-  unless client.validate_signature(body, signature)
-    halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
+  def callback
+    'OK'
   end
-
-  events = client.parse_events_from(body)
-
-  events.each do |event|
-    case event
-    when Line::Bot::Event::Message
-      case event.type
-      when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
-        client.reply_message(event['replyToken'], message)
-      end
-    end
-  end
-
-  "OK"
- end
 end
