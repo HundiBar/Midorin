30f98935fca5756ee3c042c397668cc095844bb7
Dean Abbott <deanabbott293@gmail.com>
2021-08-23 12:45:02 +0900

    basic chatroom restyled

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,11 +1,23 @@
 class ChatroomsController < ApplicationController
   def index
-    @chatrooms = policy_scope(Chatroom).order(created_at: :desc)
+    @chatrooms = policy_scope(Chatroom).order(created_at: :asc)
   end
 
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
     authorize @chatroom
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    authorize @chatroom
+    redirect_to chatrooms_path
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:title)
   end
 end
