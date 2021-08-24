881c67106e4cd37b2f909c5f597d8f63dfab1923
Dean Abbott <deanabbott293@gmail.com>
2021-08-24 16:30:43 +0900

    adding chatrooms with a create route

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,4 +3,14 @@ class Chatroom < ApplicationRecord
 
   validates :title, presence: true
   validates :title, uniqueness: true
+  chatroom_names = []
+  Chatroom.all.each do |chatroom|
+    chatroom_names << chatroom.title
+  end
+  p chatroom_names
+  CHATNAME = [];
+  Plant.all.each do |plant|
+    CHATNAME << plant.name unless chatroom_names.include? plant.name
+  end
+  p CHATNAME
 end
