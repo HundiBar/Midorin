4b91ff3231a8b39cbd3384c01596f13b6129bf1c
Dean Abbott <deanabbott293@gmail.com>
2021-08-26 21:20:27 +0900

    line end

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -7,26 +7,15 @@ class LineController < ApplicationController
     user = get_ids(events)
 
     events.each do |event|
-
       case event.type
         # when receive a text message
       when Line::Bot::Event::MessageType::Text
-
-        # if event.message['text'].downcase.include?('add')
-        #   response = LinebotAddToListJob.perform_now(event.message['text'], user, default_message)
-        # elsif event.message['text'].downcase.include?('shopping done')
-        #   response = LinebotLoadToPantryJob.perform_now(user)
-        # elsif event.message['text'].downcase.include?('list')
-        #   LinebotListGardenJob.perform_now(user.pots.all, client, user)
-        # else
         response = "I don't understand!"
-        # end
         message_hash = {
           type: 'text',
           text: response
         }
         client.reply_message(event['replyToken'], message_hash)
-
         'OK'
       end
     end
@@ -43,25 +32,26 @@ class LineController < ApplicationController
   # every time there is a call back request from the group, we find the household with group ID, find the household head
   # (for now assuming only household head has account on Pantry app) and its head user, and make changes to the pantry of that head user
 
-  events.each do |event|
-    id = event['source']['userId']
-    unless User.exists?(line_id: id)
-      client.reply_message(event['replyToken'], { type: 'text', text: 'Please sign up with LINE on www.midorin.one'})
+    events.each do |event|
+      id = event['source']['userId']
+      unless User.exists?(line_id: id)
+        client.reply_message(event['replyToken'], { type: 'text', text: 'Please sign up with LINE on www.midorin.one'})
+      end
+      user = User.find_by(line_id: id)
+
+      # case event.type
+      # when Line::Bot::Event::MessageType::Text
+      #   if event['source']['groupId']
+      #     group_line_id = event['source']['groupId']
+      #     unless Household.exists?(line_id: group_line_id)
+      #       household = Household.create(line_id: group_line_id)
+      #       user.update(household_id: household.id)  # update household LINE ID everytime this user added Pantry bot to a new group
+      #     end
+      #   end
+      # end
+
+      return user
+      'OK'
     end
-    user = User.find_by(line_id: id)
-
-    # case event.type
-    # when Line::Bot::Event::MessageType::Text
-    #   if event['source']['groupId']
-    #     group_line_id = event['source']['groupId']
-    #     unless Household.exists?(line_id: group_line_id)
-    #       household = Household.create(line_id: group_line_id)
-    #       user.update(household_id: household.id)  # update household LINE ID everytime this user added Pantry bot to a new group
-    #     end
-    #   end
-    # end
-
-    return user
-    'OK'
   end
 end
