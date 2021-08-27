b43fd49503f6894e14bb5a022db7a128d13776f1
Dean Abbott <deanabbott293@gmail.com>
2021-08-27 14:33:03 +0900

    spinner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_08_26_060051) do
 
   # These are extensions that must be enabled in order to support this database
@@ -65,7 +64,6 @@ ActiveRecord::Schema.define(version: 2021_08_26_060051) do
     t.string "temperature"
     t.string "light"
     t.string "toxicity"
-    t.integer "hours_to_water"
     t.integer "days_until_water"
   end
 
@@ -91,10 +89,10 @@ ActiveRecord::Schema.define(version: 2021_08_26_060051) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "line_id"
-    t.string "provider"
     t.string "water"
     t.string "light"
+    t.string "line_id"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
