f2d762595d7e6f75d594e01807e3cc2231196337
Spencer Mattison <spenceramattison@gmail.com>
2021-08-31 20:05:36 +0900

    removed unneed code

--- app/jobs/linebot_garden_job.rb
+++ app/jobs/linebot_garden_job.rb
@@ -1,7 +1,6 @@
 class LinebotGardenJob < ApplicationJob
 
   def perform(client, user)
-    # user_pots = user.pots
     linebot = LineBot.new(user.line_id)
     destination = user.line_id
     message_text = user.pots.empty? ? "You have no plants!" : "Here's your Midorin Garden," "\n\n#{load_pots(user.pots).join("\n")}"
@@ -10,7 +9,6 @@ class LinebotGardenJob < ApplicationJob
       text: message_text
     }
     linebot.client.push_message(destination, message)
-
   end
 
   private
@@ -20,42 +18,5 @@ class LinebotGardenJob < ApplicationJob
       "#{pot.nickname || pot.plant.name} is doing good. \n#{pot.notification}#{pot[-1] ? "" : "\n"}"
     end
   end
-
-  # def notification
-
-  #   if water_in_days == 0
-  #     message = "You need to water #{nickname? ? nickname : plant.name} today."
-  #   elsif water_in_days <= -1
-  #     message = "It has been #{water_in_days * -1} days since #{nickname? ? nickname : plant.name} has been watered"
-  #   elsif water_in_days == 1
-  #     message = "You need to water #{nickname? ? nickname : plant.name} tomorrow."
-  #   else
-  #     message = "#{nickname? ? nickname : plant.name} doesn't need to be watered today!"
-  #   end
-
-  # end
 end
 
-# class LinebotLoadToPantryJob < ApplicationJob
-
-#   def perform(user)
-#     shopping_list = user.shopping_lists.last
-#     return 'You have no shopping lists!' if shopping_list.nil?
-
-#     shopping_amounts = shopping_list.shopping_amounts
-#     load_to_pantry(shopping_amounts, user)
-
-#     shopping_list.destroy
-#   end
-
-#   private
-
-#   def load_to_pantry(shopping_amounts, user)
-#     shopping_amounts.each do |shopping_amount|
-#       pantry_item = ItemAmount.new(item_id: shopping_amount.item.id)
-#       pantry_item.user = user
-#       pantry_item.description = shopping_amount.description
-#       pantry_item.save
-#     end
-#   end
-# end
