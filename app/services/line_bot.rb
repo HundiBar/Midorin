c358f38087c1486ae6337adc8d67219dfd5b4d00
Spencer Mattison <spenceramattison@gmail.com>
2021-08-31 21:48:42 +0900

    deleted unneeded code and changed wording to notification.

--- app/services/line_bot.rb
+++ app/services/line_bot.rb
@@ -34,22 +34,4 @@ class LineBot
   end
 
 end
-  # def bot_answer_to(a_question, user_name)
-  # # Only answer to messages with "bob"
-  #   if a_question.match?(/say (hello|hi) to/i)
-  #     "Hello #{a_question.match(/say (hello|hi) to (.+)\b/i)[2]}!!"
-  #   elsif a_question.match?(/(Hi|Hey|Bonjour|Hi there|Hey there|Hello).*/i)
-  #     "Hello " + user_name + ", how are you doing today?"
-  #   elsif a_question.match?(/how\s+.*are\s+.*you.*/i)
-  #     "I am fine, " + user_name
-  #   elsif a_question.match?(/how\s+.*are\s+.*you.*/i)
-  #     "I am fine, " + user_name
-  #   elsif a_question.end_with?('?')
-  #     "Good question, " + user_name + "!"
-  #   elsif a_question.match?("#{Pot.nickname}")
-  #     "#{Pot.nickname} is doing well."
-  #   else
-  #     ["I couldn't agree more.", "Great to hear that.", "Kinda makes sense."].sample
-  #   end
-  # end
 
